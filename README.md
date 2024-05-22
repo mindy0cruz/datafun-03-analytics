af02613a3e1275665ea2fca29b7dd104140c4193
Mindy Cruz <mindy_cruz@icloud.com>
2024-05-21 19:37:21 -0500

    add .gitignore, cmds to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # datafun-03-analytics
 Project 3
+Create a new GitHub repository with a default README.md and the required name. After creating it, use git to clone it down to your machine.
+git clone https://github.com/mindy0cruz/datafun-03-analytics
+
---
+++ mindycruz_analytics.py
@@ -0,0 +1,1 @@
+# Project 3 emphasizes skills in using Git for version control, creating and managing Python virtual environments, and handling different types of data. The project involves fetching data from the web, processing it using appropriate Python collections, and writing the processed data to files.
